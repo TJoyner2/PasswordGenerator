5d3c4d954c09f34acdf84a1e3c49f435363541fa
Trent <trent.joyner.gmail.com>
2024-01-05 20:37:25 -0500

    added the function that puts perameters around how many chreachters you can have in your password.

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ let big = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
 let little = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let number = [0,1,2,3,4,5,6,7,8,9];
 let special = ['!','@','#','$','%','^','&', "*", '(', ')'];
+
+function generatePassword(){
+    let password = "";
+    let passwordCon = [];
+
+let passwordLength;
+    passwordLength = promt("Please type a password length from 8-128 characters");
+        if (passwordLength<8 || passwordLength > 128) {
+            alert ("Please make password between 8-128 characters");
+            return
+        }
+} 
