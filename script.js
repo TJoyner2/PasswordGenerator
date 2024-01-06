8d2d1e04d4e455c4ec9c17f95bd43bd68385f8c3
Trent <trent.joyner.gmail.com>
2024-01-05 20:26:08 -0500

    created my button and listed my variables

---
+++ script.js
@@ -0,0 +1,5 @@
+var generateBtn = document.querySelector('#generate')
+let big = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let little = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let number = [0,1,2,3,4,5,6,7,8,9];
+let special = ['!','@','#','$','%','^','&', "*", '(', ')'];
